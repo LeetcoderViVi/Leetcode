d6f0199440b58f630f530596578dbdf4a8a38fb1
LeetcoderViVi <fww00001@126.com>
2015-05-20 10:19:01 +0800

    Update Pascal's Triangle.cpp

--- Pascal's Triangle.cpp
+++ Pascal's Triangle.cpp
@@ -3,20 +3,20 @@ public:
     vector<vector<int>> generate(int numRows) {
         /*if(numRows==0)
 			return;*/
-		vector<vector<int>> vvi;
-		vector<int> vi;
-		for(int i=1;i<=numRows;i++)
+	vector<vector<int>> vvi;
+	vector<int> vi;
+	for(int i=1;i<=numRows;i++)
+	{
+		for(int j=1;j<=i;j++)
 		{
-			for(int j=1;j<=i;j++)
-			{
-				if(j==1||j==i)
-					vi.push_back(1);
-				else
-					vi.push_back(vvi[i-2][j-2]+vvi[i-2][j-1]);
-			}
-			vvi.push_back(vi);
-			vi.clear();
+			if(j==1||j==i)
+				vi.push_back(1);
+			else
+				vi.push_back(vvi[i-2][j-2]+vvi[i-2][j-1]);
 		}
-		return vvi;
+		vvi.push_back(vi);
+		vi.clear();
+	}
+	return vvi;
     }
 };
