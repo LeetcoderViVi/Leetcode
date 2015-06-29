70672a59dae2fddd23d3d1b38e056d70a58190cc
LeetcoderViVi <fww00001@126.com>
2015-06-29 20:28:42 +0800

    Update Integer to Roman.cpp

--- Integer to Roman.cpp
+++ Integer to Roman.cpp
@@ -1,3 +1,4 @@
+//Solution 1.
 class Solution {
 public:
     string intToRoman(int num) {
@@ -60,3 +61,6 @@ public:
 		return result;
     }
 };
+// Solution 2.
+String[] romanPieces={"","I","II","III","IV","V","VI","VII","VIII","IX", "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC", "","C","CC","CCC","CD","D","DC","DCC","DCCC","CM", "","M","MM","MMM","MMMM"}; 
+return romanPieces[num/1000+30]+romanPieces[(num/100)%10+20] +romanPieces[(num/10)%10+10]+romanPieces[num%10];
