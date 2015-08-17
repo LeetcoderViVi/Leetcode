8dfb3420699766ca23578eb7fd05574029ff3a97
LeetcoderViVi <fww00001@126.com>
2015-08-17 22:34:34 +0800

    Create Tree:Minimum Depth of Binary Tree.cpp

---
+++ Tree:Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,18 @@
+//min is different from max!!!
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        if(!root->left){
+            return 1 + minDepth(root->right);
+        }
+        if(!root->right){
+            return 1 + minDepth(root->left);
+        }
+        
+        return min(minDepth(root->left),minDepth(root->right)) + 1;
+    }
+};
